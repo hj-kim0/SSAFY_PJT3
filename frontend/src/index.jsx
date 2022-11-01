569a3f24abe05127bfb6bee2c623aab3dc8395dd
김혁주 <lyricism3002@gmail.com>
2022-11-02 00:43:04 +0900

    revert test

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> parent of ee91941 (Frontend # feat : home/common struct create & refactoring)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
