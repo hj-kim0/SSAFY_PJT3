e60cc9310ba2876fca13acd068f3419767e9f552
김혁주 <lyricism3002@gmail.com>
2022-11-04 14:59:52 +0900

    Frontend # fix : 컨펌내용반영) 메인페이지 이미지 축소, 푸터 크기 조절

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -6,23 +6,24 @@ function FooterPage(){
     return(
         <footer className="footer">
             <div className="footer_wrapper">
-            <div className="footer_body">
-                <div className="pd3">
-                    <h1>
+            <div className="footer_body flex">
+                <div className="flex">
+                <h5 className="fs-20">
                     Contact Us
-                    </h1>
-                </div>
-                <div className="pd3">
+                    </h5>
+                <div className="pdl-10">
                     <a href="https://www.notion.so/Comssa-42d6c98b7d044b2687a597035b1777ee">
-                        <img src={notion} width="50px" alt=""/>
+                        <img src={notion} width="20px" alt=""/>
                     </a>
                 </div>
-                <div className="pd3">
-                <span className='white'>&copy; {new Date().getFullYear()} Copyright:{' '}</span>
-                <a className='white' href='https://edu.ssafy.com/'>
+                <div className="pdl-10"><span className='white fs-10'>&copy; {new Date().getFullYear()} Copyright:{' '}</span>
+                <a className='white fs-10' href='https://edu.ssafy.com/'>
                     SSAFY.com
-                </a>
-            </div>
+                </a></div>
+                </div>
+                </div>
+                <div>
+                
             </div>
             </div>
         </footer>
