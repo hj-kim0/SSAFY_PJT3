23826f96334c1fd89609f5a9541f36eabbf7af68
1992JYP <37524453+1992JYP@users.noreply.github.com>
2022-11-08 10:41:11 +0900

    find all api clear

---
+++ backend/src/main/java/com/ssafy/comssa/service/part/SsdService.java
@@ -0,0 +1,44 @@
+package com.ssafy.comssa.service.part;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.comssa.repository.part.SSDRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@SuppressWarnings("unused")
+public class SsdService {
+
+    @Autowired
+    SSDRepository ssdRepository;
+
+    public String selectSsd(String name) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        log.info("hello========================================");
+        try {
+//            log.info(objectMapper.writeValueAsString(cpuRepository.findByPartsID(name)));
+            return objectMapper.writeValueAsString(ssdRepository.findAll());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+    }
+
+//    public void saveUser(String name) {
+//
+//        Cpu cpu = new Cpu();
+////        cpu.setName(name);
+////        cpu.setPrice(price);
+//
+//        if (cpuRepository.findByPartsID(name) != null)
+//            log.info("saved!!!");
+////            cpu.setId(cpuRepository.findByName(name).getId());
+//
+//
+//        cpuRepository.save(cpu);
+//    }
+
+}
