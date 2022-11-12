9e7b82286c09df3c5849961d7ab929e179e21770
김혁주 <lyricism3002@gmail.com>
2022-11-12 12:18:24 +0900

    fix

--- frontend/src/components/common/AppHeader.jsx
+++ frontend/src/components/common/AppHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './AppHeader.scss';
 import logo from "../../assets/images/common/logo.png";
