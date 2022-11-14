f1ff7cfd01c51d2d069cb9379e3b9bfabedc9492
오승준 <bee0665@hanmail.net>
2022-11-14 16:21:51 +0900

    frontend # feat : master애 guide 페이지 추가

--- frontend/src/routers/Router.jsx
+++ frontend/src/routers/Router.jsx
@@ -6,9 +6,9 @@ import Login from "../screens/user/login/Login";
 import Profile from "../screens/user/profile/Profile";
 //
 import { getCurrentUser } from "../utils/APIUtils";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
-import OAuth2RedirectHandler from '../screens/user/oauth2/OAuth2RedirectHandler';
-import Alert from 'react-s-alert';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import OAuth2RedirectHandler from "../screens/user/oauth2/OAuth2RedirectHandler";
+import Alert from "react-s-alert";
 
 import AppHeader from "../components/common/AppHeader";
 import Footer from "../components/common/Footer";
@@ -16,34 +16,31 @@ import Footer from "../components/common/Footer";
 import Compare from "../screens/Compare";
 import Estimate from "../screens/Estimate";
 import Search from "../screens/Search";
+import Guide from "../screens/Guide";
 
 import "./Router.scss";
 import AutoManual from "../screens/AutoManual";
 import Loading from "../components/effect/Loading";
 
 function Router() {
-
   const [loading, setLoading] = useState(true);
 
-  const [state, setState] = useState(
-    {
-      authenticated: false,
-      currentUser: null,
-    }
-  );
-  
+  const [state, setState] = useState({
+    authenticated: false,
+    currentUser: null,
+  });
+
   function loadCurrentlyLoggedInUser() {
     getCurrentUser()
-    .then(
-      response => {
+      .then((response) => {
         setState({
           authenticated: true,
           currentUser: response,
         });
 
         setLoading(false);
-
-      }).catch(err => {
+      })
+      .catch((err) => {
         setState({
           authenticated: false,
           currentUser: null,
@@ -54,47 +51,48 @@ function Router() {
   }
 
   function handleLogout() {
-      localStorage.removeItem(ACCESS_TOKEN);
-      localStorage.removeItem(REFRESH_TOKEN);
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
 
-      setState({
-        authenticated: false,
-        currentUser: null,
-      });
+    setState({
+      authenticated: false,
+      currentUser: null,
+    });
 
-      setLoading(true);
-      Alert.success("로그아웃 했습니다.")
+    setLoading(true);
+    Alert.success("로그아웃 했습니다.");
   }
 
   useEffect(() => {
     loadCurrentlyLoggedInUser();
-  },[]);
+  }, []);
 
-    return (
-      <>
-        <div id="wrapper">
-          { loading ? <Loading/> : null }
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            {/* user */}
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler/>}></Route>  
-            <Route path="/profile" element={<Profile/>}></Route>
-            {/* comssa */}
-            <Route path='/compare' element={<Compare />}></Route>
-            <Route path='/auto' element={<AutoManual />}></Route>
-            <Route path='/estimate' element={<Estimate/>}></Route>
-            <Route path='/search' element={<Search/>}></Route>
-          </Routes>
-          <div id="nav_wrapper">
-            <AppHeader data={state} 
-            handleLogout={handleLogout}
-            />
-          </div>
-          <Footer />
+  return (
+    <>
+      <div id="wrapper">
+        {loading ? <Loading /> : null}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          {/* user */}
+          <Route path="/login" element={<Login />}></Route>
+          <Route
+            path="/oauth2/redirect"
+            element={<OAuth2RedirectHandler />}
+          ></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          {/* comssa */}
+          <Route path="/compare" element={<Compare />}></Route>
+          <Route path="/auto" element={<AutoManual />}></Route>
+          <Route path="/estimate" element={<Estimate />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/guide" element={<Guide />}></Route>
+        </Routes>
+        <div id="nav_wrapper">
+          <AppHeader data={state} handleLogout={handleLogout} />
         </div>
-      </>
-    );
-
+        <Footer />
+      </div>
+    </>
+  );
 }
 export default Router;
