33788c553aa7aa4d571a180a862efbfa8651e801
오승준 <bee0665@hanmail.net>
2022-11-16 14:59:10 +0900

    frontend # feat : 정보 페이지 추가(기능 미구현)

---
+++ frontend/src/screens/UserInfo.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import ComAdd from "../assets/images/login/Add.png";
+import ComDelete from "../assets/images/login/Delete.png";
+import ProfilePicture from "../assets/images/common/user.png";
+import ComputerAdd from "../assets/images/common/Computer_Add.png";
+
+import "./UserInfo.scss";
+
+export default function UserInfo() {
+  return (
+    <div className="info_body">
+      <div className="info_user">
+        <h1>
+          <b>회원 정보</b>
+        </h1>
+        <br />
+        <img
+          src={ProfilePicture}
+          id="info_picture"
+          alt="이 곳에 프로필 사진이 들어갑니다"
+        />
+        <h2>
+          <b>이곳에 자신의 닉네임을 표시합니다.</b>
+        </h2>
+      </div>
+      <div className="info_pc">
+        <div>
+          <button id="add_button">
+            <img
+              id="pc_add"
+              src={ComputerAdd}
+              alt="견적을 맞춘 내 컴퓨터를 추가하려면 클릭"
+            />
+          </button>
+        </div>
+        <div>
+          <p style={{ display: "inline" }}></p>
+          <button id="mypc_list">내 컴퓨터</button>
+          <button>
+            <img
+              src={ComAdd}
+              alt="짜여진 견적을 추가하려면 클릭"
+              id="add_but"
+            />
+          </button>
+          <button>
+            <img
+              src={ComDelete}
+              alt="짜여진 견적을 삭제하려면 클릭"
+              id="sub_but"
+            />
+          </button>
+        </div>
+      </div>
+      <div>
+        <button type="EditComplete" id="button_complete">
+          수정 완료
+        </button>
+      </div>
+    </div>
+  );
+}
