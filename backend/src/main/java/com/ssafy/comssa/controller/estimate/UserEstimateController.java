250b741c2bc6884dcb21b954f982ccda5ce79c09
1992JYP <37524453+1992JYP@users.noreply.github.com>
2022-11-20 21:52:15 +0900

    UserEstimateController post call update

--- backend/src/main/java/com/ssafy/comssa/controller/estimate/UserEstimateController.java
+++ backend/src/main/java/com/ssafy/comssa/controller/estimate/UserEstimateController.java
@@ -41,9 +41,111 @@ public class UserEstimateController extends PartsFindController {
         log.info(forReturnString);
         log.info("c=========lear");
         ArrayList<JSONObject> returnArrayList = (JSONArray) parser.parse(forReturnString);
+        log.info("============arraylistk return");
+        log.info(returnArrayList.toString());
 
+        ArrayList<JSONObject> returnJsonList = new ArrayList<JSONObject>();
+        int count =0;
         for (JSONObject j:returnArrayList) {
-            log.info(j.toString());
+            JSONObject inputJson = new JSONObject();
+            ArrayList<String> partsString = (ArrayList<String>) j.get("parts");
+            inputJson.put("userID",j.get("userID"));
+            inputJson.put("estimateName",j.get("estimateName"));
+            inputJson.put("idx",count++);
+
+            String cpuFind;
+            if (partsString.get(0) ==null)
+                cpuFind ="none";
+            else
+                cpuFind = findCpuData(partsString.get(0));
+
+            String gpuFind;
+            if (partsString.get(3) ==null)
+                gpuFind ="none";
+            else
+                gpuFind = findGpuData(partsString.get(3));
+
+            String mainboardFind;
+            if (partsString.get(1) ==null)
+                mainboardFind ="none";
+            else
+                mainboardFind = findMainboardData(partsString.get(1));
+
+            String memoryFind;
+            if (partsString.get(2) ==null)
+                memoryFind ="none";
+            else
+                memoryFind = findMainboardData(partsString.get(2));
+
+            String ssdFind;
+            if (partsString.get(4) ==null)
+                ssdFind ="none";
+            else
+                ssdFind = findSsdData(partsString.get(4));
+
+            String powerFind;
+            if (partsString.get(5) ==null)
+                powerFind ="none";
+            else
+                powerFind = findPowerData(partsString.get(5));
+
+            String coolerFind;
+            if (partsString.get(6) ==null)
+                coolerFind ="none";
+            else
+                coolerFind = findCoolerData(partsString.get(6));
+
+            String towerFind;
+            if (partsString.get(7) ==null)
+                towerFind ="none";
+            else
+                towerFind = findCoolerData(partsString.get(7));
+
+            JSONObject cpuJsonObject = (cpuFind.equals("none")) ? null : (JSONObject) parser.parse(cpuFind);
+            JSONObject gpuJsonObject = (gpuFind.equals("none")) ? null : (JSONObject) parser.parse(gpuFind);
+            JSONObject mainboardJsonObject = (mainboardFind.equals("none"))  ? null : (JSONObject) parser.parse(mainboardFind);
+            JSONObject memoryJsonObject = (memoryFind.equals("none"))  ? null : (JSONObject) parser.parse(memoryFind);
+            JSONObject powerJsonObject = (powerFind.equals("none"))  ? null : (JSONObject) parser.parse(powerFind);
+            JSONObject ssdJsonObject = (ssdFind.equals("none"))  ? null : (JSONObject) parser.parse(ssdFind);
+            JSONObject towerJsonObject = (towerFind.equals("none")) ? null : (JSONObject) parser.parse(towerFind);
+            JSONObject coolerJsonObject = (coolerFind.equals("none")) ? null : (JSONObject) parser.parse(coolerFind);
+
+            ArrayList<JSONObject> returnArray = new ArrayList<>();
+            JSONObject jsonObject0 = new JSONObject();
+            jsonObject0.put("idx", 0);
+            jsonObject0.put("Detail", cpuJsonObject);
+            returnArray.add(jsonObject0);
+            JSONObject jsonObject1 = new JSONObject();
+            jsonObject1.put("idx", 1);
+            jsonObject1.put("Detail", mainboardJsonObject);
+            returnArray.add(jsonObject1);
+            JSONObject jsonObject2 = new JSONObject();
+            jsonObject2.put("idx", 2);
+            jsonObject2.put("Detail", memoryJsonObject);
+            returnArray.add(jsonObject2);
+            JSONObject jsonObject3 = new JSONObject();
+            jsonObject3.put("idx", 3);
+            jsonObject3.put("Detail", gpuJsonObject);
+            returnArray.add(jsonObject3);
+            JSONObject jsonObject4 = new JSONObject();
+            jsonObject4.put("idx", 4);
+            jsonObject4.put("Detail", ssdJsonObject);
+            returnArray.add(jsonObject4);
+            JSONObject jsonObject5 = new JSONObject();
+            jsonObject5.put("idx", 5);
+            jsonObject5.put("Detail", powerJsonObject);
+            returnArray.add(jsonObject5);
+            JSONObject jsonObject6 = new JSONObject();
+            jsonObject6.put("idx", 6);
+            jsonObject6.put("Detail", coolerJsonObject);
+            returnArray.add(jsonObject6);
+            JSONObject jsonObject7 = new JSONObject();
+            jsonObject7.put("idx", 7);
+            jsonObject7.put("Detail", towerJsonObject);
+            returnArray.add(jsonObject7);
+
+            inputJson.put("parts",returnArray);
+            returnJsonList.add(inputJson);
         }
 //        JSONObject jsonObject = (JSONObject) parser.parse(forReturnString);
 
@@ -52,7 +154,7 @@ public class UserEstimateController extends PartsFindController {
 
 //        ArrayList<JSONObject> returnArrayList = new ArrayList<>();
 
-        return returnArrayList;
+        return returnJsonList;
     }
 
     @PutMapping(value = "/put")
